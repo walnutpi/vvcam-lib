445f6ce46ca7b5ba5bc780a036a5d58ca1c8711e
sc-bin <3335447573@qq.com>
2026-07-09 13:51:51 +0800

    优化启动isp的脚本，指定isp可执行文件路径在本项目下

--- run_isp_media.sh
+++ run_isp_media.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 运行lsmod查看当前加载的内核模块，判断是否有vvcam_isp模块
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 modprobe vvcam_isp
 modprobe vvcam_mipi
@@ -9,6 +9,6 @@ modprobe vvcam_isp_subdev
 modprobe vvcam_video
 
 chmod 777 /proc/vsi/isp_subdev0
-ISP_MEDIA_SENSOR_DRIVER=/usr/lib/libvvcam.so /usr/bin/isp_media_server_debian >/tmp/isp.err.log 2>&1 &
+ISP_MEDIA_SENSOR_DRIVER=/usr/lib/libvvcam.so "${SCRIPT_DIR}/isp_media_server_debian" >/tmp/isp.err.log 2>&1 &
 sleep 1
 python3 -c "import k230_sensor; cap = k230_sensor.Sensor(1920, 1080)" &
